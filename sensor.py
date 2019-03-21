e5c055f5c1394b48b48c397fbb8b30bac9f5b1d6
Rob <rob.a.landry@gmail.com>
2019-03-21 19:05:55 -0400

    More features.

--- sensor.py
+++ sensor.py
@@ -28,78 +28,69 @@ _LOGGER = logging.getLogger(__name__)
 DEFAULT_HOST = '192.168.1.1'
 DEFAULT_PORT = '5000'
 
-MIN_TIME_BETWEEN_UPDATES = timedelta(minutes=1)
+MIN_TIME_BETWEEN_UPDATES = timedelta(minutes=5)
 
 SENSOR_TYPES = {
-    'info': [
-        'Info', 'ModelName',
-        'mdi:router-wireless', 'getInfo'],
     'check_fw': [
         'Firmware', 'CurrentVersion',
         'mdi:cellphone-link', 'check_new_firmware'],
-    'attached_devices': [
-        'Attached Devices', '',
-        'mdi:cellphone-link', 'get_attached_devices'],
-    'attached_devices2': [
-        'Attached Devices2', '',
-        'mdi:cellphone-link', 'get_attached_devices_2'],
     'traffic_meter': [
         'Traffic Meter', 'NewTodayConnectionTime',
         'mdi:chart-areaspline', 'get_traffic_meter_statistics'],
-    'support_feature': [
-        'Supported Features', '',
-        'mdi:router-wireless-settings', 'getSupportFeatureListXML'],
-    'bw_control': [
-        'Bandwidth Control', '',
-        'mdi:router-wireless-settings', 'get_bandwidth_control_options'],
-    'qos_enabled': [
-        'QOS Enabled', 'NewQoSEnableStatus',
-        'mdi:router-wireless-settings', 'getQoSEnableStatus'],
-    'mac_address': [
-        'All MAC Addresses', '',
-        'mdi:router-wireless-settings' 'get_all_mac_addresses'],
-    'dns_masq': [
-        'DNS Masq', '',
-        'mdi:router-wireless-settings', 'get_dns_masq_device_id'],
     'traffic_meter_enabled': [
         'Traffic Meter Enabled', 'NewTrafficMeterEnable',
         'mdi:chart-areaspline', 'get_traffic_meter_enabled'],
     'traffic_meter_options': [
         'Traffic Meter Opt', 'NewControlOption',
         'mdi:chart-areaspline', 'get_traffic_meter_options'],
+    'parental_control_status': [
+        'Parental Control Enabled', 'ParentalControl',
+        'mdi:router-wireless-settings', 'get_parental_control_enable_status'],
+    'mac_address': [
+        'All MAC Addresses', '',
+        'mdi:router-wireless-settings', 'get_all_mac_addresses'],
+    'dns_masq': [
+        'DNS Masq', '',
+        'mdi:router-wireless-settings', 'get_dns_masq_device_id'],
+    'info': [
+        'Info', 'ModelName',
+        'mdi:router-wireless', 'getInfo'],
+    'support_feature': [
+        'Supported Features', '',
+        'mdi:router-wireless-settings', 'getSupportFeatureListXML'],
+    'speed_test_result': [
+        'Speed Test Result', 'NewOOKLADownlinkBandwidth',
+        'mdi:speedometer', 'get_speed_test_result', 'Mbps'],
+    'qos_enabled': [
+        'QOS Enabled', 'NewQoSEnableStatus',
+        'mdi:router-wireless-settings', 'getQoSEnableStatus'],
+    'bw_control': [
+        'Bandwidth Control', '',
+        'mdi:router-wireless-settings', 'get_bandwidth_control_options'],
     'guest_access': [
         'Guest Access', 'NewGuestAccessEnabled',
         'mdi:account-group', 'get_guest_access_enabled'],
     'guest_access_5g': [
-        '5G1 Guest Access', 'NewGuestAccessEnabled',
-        'mdi:account-group', 'get_5g1_guest_access_enabled'],
-    'guest_access_5g1': [
-        '5G1 Guest Access2', 'NewGuestAccessEnabled',
-        'mdi:account-group', 'get_5g1_guest_access_enabled_2'],
-    'guest_access_5g2': [
-        '5G1 Guest Access2', 'NewGuestAccessEnabled',
-        'mdi:account-group', 'get_5g_guest_access_enabled_2'],
+        '5G Guest Access', 'NewGuestAccessEnabled',
+        'mdi:account-group', 'get_5g_guest_access_enabled'],
     'wpa_key': [
         'WPA Security Key', 'NewWPAPassphrase',
         'mdi:security', 'get_wpa_security_keys'],
     'wpa_key_5g': [
         '5G WPA Security Key', 'NewWPAPassphrase',
         'mdi:security', 'get_5g_wpa_security_keys'],
-    'get_5g_info': [
-        '5G Info', 'NewSSID',
-        'mdi:signal-5g', 'get_5g_info'],
     'get_2g_info': [
         '2G Info', 'NewSSID',
         'mdi:signal-2g', 'get_2g_info'],
+    'get_5g_info': [
+        '5G Info', 'NewSSID',
+        'mdi:signal-5g', 'get_5g_info'],
     'guest_access_net': [
         'Guest Network Info', 'NewSSID',
         'mdi:signal-2g', 'get_guest_access_network_info'],
     'guest_access_net_5g': [
         '5G Guest Network Info', 'NewSSID',
         'mdi:signal-5g', 'get_5g_guest_access_network_info'],
-    'speed_test_result': [
-        'Speed Test Result', 'NewOOKLADownlinkBandwidth',
-        'mdi:speedometer', 'get_speed_test_result'],
 }
 
 
@@ -124,7 +115,7 @@ def setup_platform(hass, config, add_devices, discovery_info=None):
     ssl = config[CONF_SSL]
     resources = config[CONF_RESOURCES]
 
-    _LOGGER.info("NETGEAR: setup_platform")
+    _LOGGER.debug("NETGEAR: setup_platform")
 
     sensors = []
     for kind in resources:
@@ -138,10 +129,17 @@ class NetgearEnhancedSensor(Entity):
 
     def __init__(self, host, ssl, username, password, port, kind):  # noqa
         """Initialize the sensor."""
-        # self._entity_id = ("ng_enhanced_%s", kind)
-        self._name = SENSOR_TYPES[kind][0]
+        #if isinstance(kind, str):
+        #    self._unique_id = "ng_enhanced_%s" % (kind)
+        #else:
+        #    self._unique_id = None
+        #_LOGGER.debug("ng_enhanced_%s", kind)
+        self._name = f"NG {SENSOR_TYPES[kind][0]}"
         self._state = None
-        self._unit_of_measurement = ''
+        if len(SENSOR_TYPES[kind]) > 4:
+            self._unit_of_measurement = SENSOR_TYPES[kind][4]
+        else:
+            self._unit_of_measurement = ''
         self._state_key = SENSOR_TYPES[kind][1]
         self._attrs = None
         self._icon = SENSOR_TYPES[kind][2]
@@ -150,9 +148,9 @@ class NetgearEnhancedSensor(Entity):
         from pynetgear_enhanced import Netgear
         self._api = Netgear(password, host, username, port, ssl)
 
-    # def entity_id(self):
-       # """Return the entity_id of the sensor."""
-       # return self._entity_id
+    # def unique_id(self):
+    #    """Return the _unique_id of the sensor."""
+    #    return self._unique_id
 
     @property
     def name(self):
@@ -187,8 +185,11 @@ class NetgearEnhancedSensor(Entity):
         """
         self.response = getattr(self._api, self._function)()
 
-        self._attrs = self.response
-        if self._state_key:
-            self._state = self.response[self._state_key]
+        if self.response: 
+            if len(self.response) > 1:
+                self._attrs = self.response
+
+            if self._state_key:
+                self._state = self.response[self._state_key]
         else:
             self._state = None
